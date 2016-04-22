2eba12a45de2a17769cec2a99bbb125ce8f2fde9
44x1carbon <b4079@oic.jp>
2016-04-22 14:15:17 +0900

    init

---
+++ 1/answer.blade.php
@@ -0,0 +1,3 @@
+<img src="http://www.geocities.jp/kenji_y0328/crenshu/renshu/r011.gif"/><br>
+出力結果：
+hogehoge
---
+++ 2/answer.blade.php
@@ -0,0 +1,1 @@
+<img src="http://www.geocities.jp/kenji_y0328/crenshu/renshu/r012.gif"/>
